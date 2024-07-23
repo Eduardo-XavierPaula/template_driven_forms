cc146e35d719f6a6a73a76578d5c1b1a91f23035
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-07-22 23:11:05 -0300

    ✨ New feature: Validadores síncronos personalizados, by Eduardo Xavier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,11 @@ import { FormsGroupComponent } from './elements/forms-group/forms-group.componen
 import { FormUserComponent } from './elements/forms-with-children/form-user/form-user.component';
 import { FormProfessionComponent } from './elements/forms-with-children/form-profession/form-profession.component';
 import { MainFormComponent } from './elements/forms-with-children/main-form/main-form.component';
+import { CustomValidatorFormEx1Component } from './custom-validators/synchronous/ex1/custom-validator-form-ex1/custom-validator-form-ex1.component';
+import { InvalidTextValidatorDirective } from './custom-validators/synchronous/ex1/directives/invalid-text-validator.directive';
 
 @NgModule({
-  declarations: [AppComponent, InputTextComponent, TextareaComponent, SelectComponent, RadioComponent, CheckboxComponent, FormsComponent, FormsGroupComponent, FormUserComponent, FormProfessionComponent, MainFormComponent],
+  declarations: [AppComponent, InputTextComponent, TextareaComponent, SelectComponent, RadioComponent, CheckboxComponent, FormsComponent, FormsGroupComponent, FormUserComponent, FormProfessionComponent, MainFormComponent, CustomValidatorFormEx1Component, InvalidTextValidatorDirective],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
